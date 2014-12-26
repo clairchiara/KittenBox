b80e413aabf9aa3af948539c03087a28a32a3a0d
clairchiara <clair.chiara@gmail.com>
2014-12-26 19:35:30 +0100

    Better random number generator and cleaned a bit

--- KittenBox/game.cpp
+++ KittenBox/game.cpp
@@ -9,7 +9,6 @@
 #include "game.h"
 
 Game::Game() {
-//	board = *new std::array<std::array<Cell, 8>, 8>;
 	for (int i = 0; i < 8; i++) {
 		for (int j = 0; j < 8; j++) {
 			board[i][j].containsKitten = false;
